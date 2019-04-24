1e8887579eefdec3ed751867e5bec3331efdda2d
Jonathan Luk <jluk87@gmail.com>
2019-04-24 21:18:18 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
 
   def index
+
+    if !params[:author].blank?
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >= ?", Time.zone.today.beginning_of_day)
+      else
+
+
+    end
+
+
     @authors = Author.all
     @posts = Post.all
   end
